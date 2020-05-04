c4393bf7caec9a6108f60b7151a77e84bf7a4090
Lienol <lawlienol@gmail.com>
2020-05-05 01:43:58 +0800

    wifi: add support for 256-QAM on 802.11n

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -904,6 +904,9 @@ return view.extend({
 					o.datatype = 'range(15,65535)';
 					o.placeholder = 100;
 					o.rmempty = true;
+					
+					o = ss.taboption('advanced', form.Flag, 'vendor_vht', _('Enable 256-QAM'), _('802.11n 2.4Ghz Only'));
+					o.default = o.disabled;
 				}
 
 
