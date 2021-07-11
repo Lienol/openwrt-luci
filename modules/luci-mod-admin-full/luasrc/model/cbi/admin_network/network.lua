e10ff77ef5fad5e0dee69abdfb15a9e7b40131a7
Lienol <23146169+Lienol@users.noreply.github.com>
2021-07-11 22:21:28 +0800

    luci: network: add packet_steering option

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/network.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/network.lua
@@ -7,6 +7,7 @@ local tpl = require "luci.template"
 local ntm = require "luci.model.network"
 local fwm = require "luci.model.firewall".init()
 local json = require "luci.jsonc"
+local utl = require "luci.util"
 
 m = Map("network", translate("Interfaces"))
 m:chain("wireless")
@@ -253,13 +254,21 @@ if fs.access("/usr/sbin/br2684ctl") then
 	m.pageaction = true
 end
 
+local packet_steering = utl.exec("cat /etc/hotplug.d/net/20-smp-packet-steering 2>/dev/null | grep 'packet_steering'")
 local network = require "luci.model.network"
-if network:has_ipv6() then
+if network:has_ipv6() or (packet_steering and packet_steering ~= "") then
 	local s = m:section(NamedSection, "globals", "globals", translate("Global network options"))
-	local o = s:option(Value, "ula_prefix", translate("IPv6 ULA-Prefix"))
-	o.datatype = "ip6addr"
-	o.rmempty = true
-	m.pageaction = true
+
+	if network:has_ipv6() then
+		local o = s:option(Value, "ula_prefix", translate("IPv6 ULA-Prefix"))
+		o.datatype = "ip6addr"
+		o.rmempty = true
+		m.pageaction = true
+	end
+
+	if (packet_steering and packet_steering ~= "") then
+		local o = s:option(Flag, "packet_steering", translate("Packet Steering"), translate("Enable packet steering across all CPUs. May help or hinder network speed."))
+	end
 end
 
 
