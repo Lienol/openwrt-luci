049e03cbede14ea572142b1411d01a8655cc5e27
Lienol <23146169+Lienol@users.noreply.github.com>
2022-04-22 22:39:04 +0800

    luci: update mkversion.sh

--- modules/luci-base/src/mkversion.sh
+++ modules/luci-base/src/mkversion.sh
@@ -10,9 +10,11 @@ if pcall(dofile, "/etc/openwrt_release") and _G.DISTRIB_DESCRIPTION then
 	distversion = _G.DISTRIB_DESCRIPTION
 	if _G.DISTRIB_REVISION then
 		distrevision = _G.DISTRIB_REVISION
+		--[[
 		if not distversion:find(distrevision,1,true) then
 			distversion = distversion .. " " .. distrevision
 		end
+		]]--
 	end
 else
 	distname    = "OpenWrt"
