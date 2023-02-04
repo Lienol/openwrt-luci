3a19cc8749d0f42e59d47461eeb871f1560a6f7b
Lienol <23146169+Lienol@users.noreply.github.com>
2023-02-04 14:35:43 +0800

    luci: add `macaddr` option to pppoe

--- modules/luci-base/luasrc/tools/proto.lua
+++ modules/luci-base/luasrc/tools/proto.lua
@@ -3,12 +3,25 @@
 
 module("luci.tools.proto", package.seeall)
 
+local uci = require("luci.model.uci").cursor()
+
 function opt_macaddr(s, ifc, ...)
 	local v = luci.cbi.Value
 	local o = s:taboption("advanced", v, "macaddr", ...)
 
 	o.placeholder = ifc and ifc:mac()
 	o.datatype    = "macaddr"
+
+	if not o.placeholder or o.placeholder == "" then
+		local uci_section = uci:get_all("network", s.section)
+		if uci_section and uci_section.ifname then
+			uci:foreach("network", "interface", function(e)
+				if e.ifname == uci_section.ifname and e.macaddr then
+					o.placeholder = e.macaddr
+				end
+			end)
+		end
+	end
 
 	function o.cfgvalue(self, section)
 		local w = ifc and ifc:get_wifinet()
@@ -24,6 +37,19 @@ function opt_macaddr(s, ifc, ...)
 		if w then
 			w:set("macaddr", value)
 		elseif value then
+			local uci_section = uci:get_all("network", s.section)
+			if uci_section and uci_section.ifname then
+				uci:foreach("network", "interface", function(e)
+					if s.section ~= e[".name"] and e.ifname == uci_section.ifname then
+						o.map:set(e[".name"], "macaddr", value)
+					end
+				end)
+				uci:foreach("network", "device", function(e)
+					if e.name == uci_section.ifname then
+						o.map:set(e[".name"], "macaddr", value)
+					end
+				end)
+			end
 			v.write(self, section, value)
 		else
 			v.remove(self, section)
@@ -31,6 +57,19 @@ function opt_macaddr(s, ifc, ...)
 	end
 
 	function o.remove(self, section)
+		local uci_section = uci:get_all("network", s.section)
+		if uci_section and uci_section.ifname then
+			uci:foreach("network", "interface", function(e)
+				if s.section ~= e[".name"] and e.ifname == uci_section.ifname then
+					o.map:del(e[".name"], "macaddr")
+				end
+			end)
+			uci:foreach("network", "device", function(e)
+				if e.name == uci_section.ifname then
+					o.map:del(e[".name"], "macaddr")
+				end
+			end)
+		end
 		self:write(section, nil)
 	end
 end
