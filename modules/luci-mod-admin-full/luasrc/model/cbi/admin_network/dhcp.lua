f57e460fb8b2e307dbe8fea3b932cf275df0860d
Lienol <23146169+Lienol@users.noreply.github.com>
2023-02-14 00:06:46 +0800

    luci: add dnsmasq `filter_a` option

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/dhcp.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/dhcp.lua
@@ -60,10 +60,13 @@ s:taboption("files", Flag, "nohosts",
 s:taboption("files", DynamicList, "addnhosts",
 	translate("Additional Hosts files")).optional = true
 	
-aaaa = s:taboption("advanced", Flag, "filter_aaaa",
-	translate("Disable IPv6 DNS forwards"),
-	translate("Filter IPv6(AAAA) DNS Query Name Resolve"))
-aaaa.optional = true
+filter_aaaa = s:taboption("advanced", Flag, "filter_aaaa",
+	translate("Filter IPv6 DNS forwards"))
+filter_aaaa.optional = true
+
+filter_a = s:taboption("advanced", Flag, "filter_a",
+	translate("Filter IPv4 DNS forwards"))
+filter_a.optional = true
 
 qu = s:taboption("advanced", Flag, "quietdhcp",
 	translate("Suppress logging"),
