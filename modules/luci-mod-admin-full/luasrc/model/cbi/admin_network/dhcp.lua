4b7376276c02bf77ffaf8ca644b5b995808df81f
Lienol <23146169+Lienol@users.noreply.github.com>
2024-07-02 19:34:30 +0800

    luci: dhcp: sync tab option with master luci

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/dhcp.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/dhcp.lua
@@ -16,21 +16,216 @@ s.anonymous = true
 s.addremove = false
 
 s:tab("general", translate("General Settings"))
+s:tab("devices", translate("Devices &amp; Ports"))
+s:tab("dnssecopt", translate("DNSSEC"))
+s:tab("filteropts", translate("Filter"))
+s:tab("forward", translate("Forwards"))
+s:tab("limits", translate("Limits"))
+s:tab("logging", translate("Log"))
 s:tab("files", translate("Resolv and Hosts Files"))
 s:tab("tftp", translate("TFTP Settings"))
-s:tab("advanced", translate("Advanced Settings"))
 
 s:taboption("general", Flag, "dns_redirect", translate("DNS Redirect"), translate("Redirect client DNS to dnsmasq"))
-
-s:taboption("general", Flag, "domainneeded",
-	translate("Domain required"),
-	translate("Don't forward <abbr title=\"Domain Name System\">DNS</abbr>-Requests without " ..
-		"<abbr title=\"Domain Name System\">DNS</abbr>-Name"))
 
 s:taboption("general", Flag, "authoritative",
 	translate("Authoritative"),
 	translate("This is the only <abbr title=\"Dynamic Host Configuration Protocol\">DHCP</" ..
 		"abbr> in the local network"))
+
+s:taboption("general", Value, "local",
+	translate("Local server"),
+	translate("Local domain specification. Names matching this domain are never forwarded and are resolved from DHCP or hosts files only"))
+
+s:taboption("general", Value, "domain",
+	translate("Local domain"),
+	translate("Local domain suffix appended to DHCP names and hosts file entries"))
+
+s:taboption("general", Flag, "expandhosts",
+	translate("Expand hosts"),
+	translate("Add local domain suffix to names served from hosts files"))
+
+se = s:taboption("general", Flag, "sequential_ip",
+	translate("Allocate IP sequentially"),
+	translate("Allocate IP addresses sequentially, starting from the lowest available address"))
+se.optional = true
+
+s:taboption("general", Flag, "allservers",
+	translate("Use all servers"),
+	translate("Setting this flag forces dnsmasq to send all queries to all available servers. The reply from the server which answers first will be returned to the original requester."))
+
+
+o = s:taboption("devices", Flag, "nonwildcard",
+	translate("Non-wildcard"),
+	translate("Bind only to specific interfaces rather than wildcard address."))
+o.optional = false
+o.rmempty = false
+
+o = s:taboption("devices", DynamicList, "interface",
+	translate("Listen Interfaces"),
+	translate("Limit listening to these interfaces, and loopback."))
+o.optional = true
+o:depends("nonwildcard", true)
+
+o = s:taboption("devices", DynamicList, "notinterface",
+	translate("Exclude interfaces"),
+	translate("Prevent listening on these interfaces."))
+o.optional = true
+o:depends("nonwildcard", true)
+
+pt = s:taboption("devices", Value, "port",
+	translate("<abbr title=\"Domain Name System\">DNS</abbr> server port"),
+	translate("Listening port for inbound DNS queries"))
+pt.optional = true
+pt.datatype = "port"
+pt.placeholder = 53
+
+qp = s:taboption("devices", Value, "queryport",
+	translate("<abbr title=\"Domain Name System\">DNS</abbr> query port"),
+	translate("Fixed source port for outbound DNS queries"))
+qp.optional = true
+qp.datatype = "port"
+qp.placeholder = translate("any")
+
+
+local have_dnssec_support = luci.util.checklib("/usr/sbin/dnsmasq", "libhogweed.so")
+if have_dnssec_support then
+	o = s:taboption("dnssecopt", Flag, "dnssec",
+		translate("DNSSEC"))
+	o.optional = true
+
+	o = s:taboption("dnssecopt", Flag, "dnsseccheckunsigned",
+		translate("DNSSEC check unsigned"),
+		translate("Requires upstream supports DNSSEC; verify unsigned domain responses really come from unsigned domains"))
+	o.optional = true
+end
+
+
+o = s:taboption("filteropts", Flag, "domainneeded",
+		translate("Domain required"),
+		translate("Don't forward <abbr title=\"Domain Name System\">DNS</abbr>-Requests without " ..
+		"<abbr title=\"Domain Name System\">DNS</abbr>-Name"))
+
+rp = s:taboption("filteropts", Flag, "rebind_protection",
+		translate("Rebind protection"),
+		translate("Discard upstream RFC1918 responses"))
+rp.rmempty = false
+
+rl = s:taboption("filteropts", Flag, "rebind_localhost",
+		translate("Allow localhost"),
+		translate("Allow upstream responses in the 127.0.0.0/8 range, e.g. for RBL services"))
+rl:depends("rebind_protection", "1")
+
+rd = s:taboption("filteropts", DynamicList, "rebind_domain",
+		translate("Domain whitelist"),
+		translate("List of domains to allow RFC1918 responses for"))
+rd.optional = true
+rd:depends("rebind_protection", "1")
+rd.datatype = "host(1)"
+rd.placeholder = "ihost.netflix.com"
+
+o = s:taboption("filteropts", Flag, "localservice",
+		translate("Local Service Only"),
+		translate("Limit DNS service to subnets interfaces on which we are serving DNS."))
+o.optional = false
+o.rmempty = false
+
+bp = s:taboption("filteropts", Flag, "boguspriv",
+		translate("Filter private"),
+		translate("Do not forward reverse lookups for local networks"))
+bp.default = bp.enabled
+
+f2k = s:taboption("filteropts", Flag, "filterwin2k",
+		translate("Filter SRV/SOA service discovery"),
+		translate("Filters SRV/SOA service discovery, to avoid triggering dial-on-demand links.") .. '<br />' ..
+		translate("May prevent VoIP or other services from working."))
+
+filter_aaaa = s:taboption("filteropts", Flag, "filter_aaaa", translate("Filter IPv6 AAAA records"), translate("Remove IPv6 addresses from the results and only return IPv4 addresses."))
+filter_aaaa.optional = true
+
+filter_a = s:taboption("filteropts", Flag, "filter_a", translate("Filter IPv4 A records"), translate("Remove IPv4 addresses from the results and only return IPv6 addresses."))
+filter_a.optional = true
+
+https = s:taboption("filteropts", Flag, "filter_https", translate("Disable HTTPS DNS Type forwards"), translate("Filter HTTPS DNS Query Type Name Resolve"))
+https.optional = true
+
+unknown = s:taboption("filteropts", Flag, "filter_unknown", translate("Disable Unknown DNS Type forwards"), translate("Filter Unknown DNS Query Type Name Resolve"))
+unknown.optional = true
+
+lq = s:taboption("filteropts", Flag, "localise_queries",
+		translate("Localise queries"),
+		translate("Localise hostname depending on the requesting subnet if multiple IPs are available"))
+
+o = s:taboption("filteropts", Flag, "nonegcache",
+		translate("No negative cache"),
+		translate("Do not cache negative replies, e.g. for not existing domains"))
+
+bn = s:taboption("filteropts", DynamicList, "bogusnxdomain", translate("Bogus NX Domain Override"),
+		translate("List of hosts that supply bogus NX domain results"))
+bn.optional = true
+bn.placeholder = "67.215.65.132"
+
+
+df = s:taboption("forward", DynamicList, "server", translate("DNS forwardings"),
+	translate("List of <abbr title=\"Domain Name System\">DNS</abbr> " ..
+			"servers to forward requests to"))
+df.optional = true
+df.placeholder = "/example.org/10.1.2.3"
+
+s:taboption("forward", Value, "serversfile",
+	translate("Additional servers file"),
+	translate("This file may contain lines like 'server=/domain/1.2.3.4' or 'server=1.2.3.4' for"..
+	        "domain-specific or full upstream <abbr title=\"Domain Name System\">DNS</abbr> servers."))
+
+
+lm = s:taboption("limits", Value, "dhcpleasemax",
+	translate("<abbr title=\"maximal\">Max.</abbr> <abbr title=\"Dynamic Host Configuration " ..
+		"Protocol\">DHCP</abbr> leases"),
+	translate("Maximum allowed number of active DHCP leases"))
+
+lm.optional = true
+lm.datatype = "uinteger"
+lm.placeholder = translate("unlimited")
+
+em = s:taboption("limits", Value, "ednspacket_max",
+	translate("<abbr title=\"maximal\">Max.</abbr> <abbr title=\"Extension Mechanisms for " ..
+		"Domain Name System\">EDNS0</abbr> packet size"),
+	translate("Maximum allowed size of EDNS.0 UDP packets"))
+
+em.optional = true
+em.datatype = "uinteger"
+em.placeholder = 1280
+
+cq = s:taboption("limits", Value, "dnsforwardmax",
+	translate("<abbr title=\"maximal\">Max.</abbr> concurrent queries"),
+	translate("Maximum allowed number of concurrent DNS queries"))
+cq.optional = true
+cq.datatype = "uinteger"
+cq.placeholder = 150
+
+cs = s:taboption("limits", Value, "cachesize",
+	translate("Size of DNS query cache"),
+	translate("Number of cached DNS entries (max is 10000, 0 is no caching)"))
+cs.optional = true
+cs.datatype = "range(0,10000)"
+cs.placeholder = 1000
+
+o = s:taboption("limits", Value, "min_cache_ttl",
+	translate("Min cache TTL"),
+	translate("Extend short TTL values to the seconds value given when caching them. Use with caution.") ..
+	translate(" (Max 1h == 3600)"))
+o.optional = true
+o.datatype = "range(0,86400)"
+o.placeholder = 60
+
+
+s:taboption("logging", Flag, "logqueries",
+	translate("Log queries"),
+	translate("Write received DNS requests to syslog")).optional = true
+
+qu = s:taboption("logging", Flag, "quietdhcp",
+	translate("Suppress logging"),
+	translate("Suppress logging of the routine operation of these protocols"))
+qu.optional = true
 
 
 s:taboption("files", Flag, "readethers",
@@ -53,200 +248,17 @@ rf = s:taboption("files", Value, "resolvfile",
 rf:depends("noresolv", "")
 rf.optional = true
 
+s:taboption("files", Flag, "strictorder",
+	translate("Strict order"),
+	translate("<abbr title=\"Domain Name System\">DNS</abbr> servers will be queried in the " ..
+		"order of the resolvfile")).optional = true
 
 s:taboption("files", Flag, "nohosts",
 	translate("Ignore <code>/etc/hosts</code>")).optional = true
 
 s:taboption("files", DynamicList, "addnhosts",
 	translate("Additional Hosts files")).optional = true
-	
-filter_aaaa = s:taboption("advanced", Flag, "filter_aaaa",
-	translate("Filter IPv6 DNS forwards"))
-filter_aaaa.optional = true
 
-filter_a = s:taboption("advanced", Flag, "filter_a",
-	translate("Filter IPv4 DNS forwards"))
-filter_a.optional = true
-
-https = s:taboption("advanced", Flag, "filter_https",
-	translate("Disable HTTPS DNS Type forwards"),
-	translate("Filter HTTPS DNS Query Type Name Resolve"))
-https.optional = true
-
-unknown = s:taboption("advanced", Flag, "filter_unknown",
-	translate("Disable Unknown DNS Type forwards"),
-	translate("Filter Unknown DNS Query Type Name Resolve"))
-unknown.optional = true
-
-qu = s:taboption("advanced", Flag, "quietdhcp",
-	translate("Suppress logging"),
-	translate("Suppress logging of the routine operation of these protocols"))
-qu.optional = true
-
-se = s:taboption("advanced", Flag, "sequential_ip",
-	translate("Allocate IP sequentially"),
-	translate("Allocate IP addresses sequentially, starting from the lowest available address"))
-se.optional = true
-
-bp = s:taboption("advanced", Flag, "boguspriv",
-	translate("Filter private"),
-	translate("Do not forward reverse lookups for local networks"))
-bp.default = bp.enabled
-
-s:taboption("advanced", Flag, "filterwin2k",
-	translate("Filter useless"),
-	translate("Do not forward requests that cannot be answered by public name servers"))
-
-
-s:taboption("advanced", Flag, "localise_queries",
-	translate("Localise queries"),
-	translate("Localise hostname depending on the requesting subnet if multiple IPs are available"))
-
-local have_dnssec_support = luci.util.checklib("/usr/sbin/dnsmasq", "libhogweed.so")
-
-if have_dnssec_support then
-	o = s:taboption("advanced", Flag, "dnssec",
-		translate("DNSSEC"))
-	o.optional = true
-
-	o = s:taboption("advanced", Flag, "dnsseccheckunsigned",
-		translate("DNSSEC check unsigned"),
-		translate("Requires upstream supports DNSSEC; verify unsigned domain responses really come from unsigned domains"))
-	o.optional = true
-end
-
-s:taboption("general", Value, "local",
-	translate("Local server"),
-	translate("Local domain specification. Names matching this domain are never forwarded and are resolved from DHCP or hosts files only"))
-
-s:taboption("general", Value, "domain",
-	translate("Local domain"),
-	translate("Local domain suffix appended to DHCP names and hosts file entries"))
-
-s:taboption("advanced", Flag, "expandhosts",
-	translate("Expand hosts"),
-	translate("Add local domain suffix to names served from hosts files"))
-
-s:taboption("advanced", Flag, "nonegcache",
-	translate("No negative cache"),
-	translate("Do not cache negative replies, e.g. for not existing domains"))
-
-s:taboption("advanced", Flag, "allservers",
-	translate("Use all servers"),
-	translate("Setting this flag forces dnsmasq to send all queries to all available servers. The reply from the server which answers first will be returned to the original requester."))
-
-s:taboption("advanced", Value, "serversfile",
-	translate("Additional servers file"),
-	translate("This file may contain lines like 'server=/domain/1.2.3.4' or 'server=1.2.3.4' for"..
-	        "domain-specific or full upstream <abbr title=\"Domain Name System\">DNS</abbr> servers."))
-
-s:taboption("advanced", Flag, "strictorder",
-	translate("Strict order"),
-	translate("<abbr title=\"Domain Name System\">DNS</abbr> servers will be queried in the " ..
-		"order of the resolvfile")).optional = true
-
-
-bn = s:taboption("advanced", DynamicList, "bogusnxdomain", translate("Bogus NX Domain Override"),
-	translate("List of hosts that supply bogus NX domain results"))
-
-bn.optional = true
-bn.placeholder = "67.215.65.132"
-
-s:taboption("general", Flag, "logqueries",
-	translate("Log queries"),
-	translate("Write received DNS requests to syslog")).optional = true
-
-df = s:taboption("general", DynamicList, "server", translate("DNS forwardings"),
-	translate("List of <abbr title=\"Domain Name System\">DNS</abbr> " ..
-			"servers to forward requests to"))
-
-df.optional = true
-df.placeholder = "/example.org/10.1.2.3"
-
-
-rp = s:taboption("general", Flag, "rebind_protection",
-	translate("Rebind protection"),
-	translate("Discard upstream RFC1918 responses"))
-
-rp.rmempty = false
-
-
-rl = s:taboption("general", Flag, "rebind_localhost",
-	translate("Allow localhost"),
-	translate("Allow upstream responses in the 127.0.0.0/8 range, e.g. for RBL services"))
-
-rl:depends("rebind_protection", "1")
-
-
-rd = s:taboption("general", DynamicList, "rebind_domain",
-	translate("Domain whitelist"),
-	translate("List of domains to allow RFC1918 responses for"))
-rd.optional = true
-
-rd:depends("rebind_protection", "1")
-rd.datatype = "host(1)"
-rd.placeholder = "ihost.netflix.com"
-
-
-pt = s:taboption("advanced", Value, "port",
-	translate("<abbr title=\"Domain Name System\">DNS</abbr> server port"),
-	translate("Listening port for inbound DNS queries"))
-
-pt.optional = true
-pt.datatype = "port"
-pt.placeholder = 53
-
-
-qp = s:taboption("advanced", Value, "queryport",
-	translate("<abbr title=\"Domain Name System\">DNS</abbr> query port"),
-	translate("Fixed source port for outbound DNS queries"))
-
-qp.optional = true
-qp.datatype = "port"
-qp.placeholder = translate("any")
-
-
-lm = s:taboption("advanced", Value, "dhcpleasemax",
-	translate("<abbr title=\"maximal\">Max.</abbr> <abbr title=\"Dynamic Host Configuration " ..
-		"Protocol\">DHCP</abbr> leases"),
-	translate("Maximum allowed number of active DHCP leases"))
-
-lm.optional = true
-lm.datatype = "uinteger"
-lm.placeholder = translate("unlimited")
-
-
-em = s:taboption("advanced", Value, "ednspacket_max",
-	translate("<abbr title=\"maximal\">Max.</abbr> <abbr title=\"Extension Mechanisms for " ..
-		"Domain Name System\">EDNS0</abbr> packet size"),
-	translate("Maximum allowed size of EDNS.0 UDP packets"))
-
-em.optional = true
-em.datatype = "uinteger"
-em.placeholder = 1280
-
-
-cq = s:taboption("advanced", Value, "dnsforwardmax",
-	translate("<abbr title=\"maximal\">Max.</abbr> concurrent queries"),
-	translate("Maximum allowed number of concurrent DNS queries"))
-
-cq.optional = true
-cq.datatype = "uinteger"
-cq.placeholder = 150
-
-cs = s:taboption("advanced", Value, "cachesize",
-	translate("Size of DNS query cache"),
-	translate("Number of cached DNS entries (max is 10000, 0 is no caching)"))
-cs.optional = true
-cs.datatype = "range(0,10000)"
-cs.placeholder = 150
-
-minittl = s:taboption("advanced", Value, "mini_ttl",
-	translate("Minimum TTL to send to clients"),
-	translate("Modify DNS entries minimum TTL (max is 86400, 0 is no modify)"))
-minittl.optional = true
-minittl.datatype = "range(0,86400)"
-minittl.placeholder = 0
 
 s:taboption("tftp", Flag, "enable_tftp",
 	translate("Enable TFTP server")).optional = true
@@ -259,7 +271,6 @@ tr.optional = true
 tr:depends("enable_tftp", "1")
 tr.placeholder = "/"
 
-
 db = s:taboption("tftp", Value, "dhcp_boot",
 	translate("Network boot image"),
 	translate("Filename of the boot image advertised to clients"))
@@ -268,29 +279,6 @@ db.optional = true
 db:depends("enable_tftp", "1")
 db.placeholder = "pxelinux.0"
 
-o = s:taboption("general", Flag, "localservice",
-	translate("Local Service Only"),
-	translate("Limit DNS service to subnets interfaces on which we are serving DNS."))
-o.optional = false
-o.rmempty = false
-
-o = s:taboption("general", Flag, "nonwildcard",
-	translate("Non-wildcard"),
-	translate("Bind only to specific interfaces rather than wildcard address."))
-o.optional = false
-o.rmempty = false
-
-o = s:taboption("general", DynamicList, "interface",
-	translate("Listen Interfaces"),
-	translate("Limit listening to these interfaces, and loopback."))
-o.optional = true
-o:depends("nonwildcard", true)
-
-o = s:taboption("general", DynamicList, "notinterface",
-	translate("Exclude interfaces"),
-	translate("Prevent listening on these interfaces."))
-o.optional = true
-o:depends("nonwildcard", true)
 
 s = m:section(TypedSection, "host", translate("Static Leases"),
 	translate("Static leases are used to assign fixed IP addresses and symbolic hostnames to " ..
@@ -353,6 +341,7 @@ function ip.validate(self, value, section)
 end
 
 m:section(SimpleSection).template = "admin_network/lease_status"
+
 
 d = m:section(TypedSection, "domain", translate("Custom domain"),
 	translate("Define a custom domain name and the corresponding PTR record"))
