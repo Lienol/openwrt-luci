456b3cc07290b263c9ab740cdeb3763ac61c7a78
Lienol <23146169+Lienol@users.noreply.github.com>
2024-07-20 18:36:11 +0800

    luci: ifaces protocol option sync with master luci

--- protocols/luci-proto-ppp/luasrc/model/cbi/admin_network/proto_l2tp.lua
+++ protocols/luci-proto-ppp/luasrc/model/cbi/admin_network/proto_l2tp.lua
@@ -4,7 +4,7 @@
 local map, section, net = ...
 
 local server, username, password
-local ipv6, defaultroute, metric, peerdns, dns, mtu
+local ipv6, mtu
 
 
 server = section:taboption("general", Value, "server", translate("L2TP Server"))
@@ -17,6 +17,7 @@ username = section:taboption("general", Value, "username", translate("PAP/CHAP u
 password = section:taboption("general", Value, "password", translate("PAP/CHAP password"))
 password.password = true
 
+
 if luci.model.network:has_ipv6() then
 	ipv6 = section:taboption("advanced", ListValue, "ipv6",
 		translate("Obtain IPv6-Address"),
@@ -26,35 +27,6 @@ if luci.model.network:has_ipv6() then
 	ipv6:value("1", translate("Manual"))
 	ipv6.default = "auto"
 end
-
-defaultroute = section:taboption("advanced", Flag, "defaultroute",
-	translate("Use default gateway"),
-	translate("If unchecked, no default route is configured"))
-
-defaultroute.default = defaultroute.enabled
-
-
-metric = section:taboption("advanced", Value, "metric",
-	translate("Use gateway metric"))
-
-metric.placeholder = "0"
-metric.datatype    = "uinteger"
-metric:depends("defaultroute", defaultroute.enabled)
-
-
-peerdns = section:taboption("advanced", Flag, "peerdns",
-	translate("Use DNS servers advertised by peer"),
-	translate("If unchecked, the advertised DNS server addresses are ignored"))
-
-peerdns.default = peerdns.enabled
-
-
-dns = section:taboption("advanced", DynamicList, "dns",
-	translate("Use custom DNS servers"))
-
-dns:depends("peerdns", "")
-dns.datatype = "ipaddr"
-dns.cast     = "string"
 
 mtu = section:taboption("advanced", Value, "mtu", translate("Override MTU"))
 mtu.placeholder = "1500"
