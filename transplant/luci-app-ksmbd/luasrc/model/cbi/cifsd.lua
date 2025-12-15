dc42b200b74e9ab5be0ec4e1f6324c28d9ffea01
Lienol <23146169+Lienol@users.noreply.github.com>
2025-12-15 23:43:38 +0800

    luci-app-ksmbd: update from lede

--- transplant/luci-app-ksmbd/luasrc/model/cbi/cifsd.lua
+++ transplant/luci-app-ksmbd/luasrc/model/cbi/cifsd.lua
@@ -8,8 +8,7 @@ s.anonymous = true
 s:tab("general",  translate("General Settings"))
 s:tab("template", translate("Edit Template"))
 
-enabled = s:taboption("general", Flag, "enabled", translate("Enabled"))
-enabled.rmempty = false
+s:taboption("general", Value, "name", translate("Hostname"))
 
 s:taboption("general", Value, "name", translate("Hostname"))
 
