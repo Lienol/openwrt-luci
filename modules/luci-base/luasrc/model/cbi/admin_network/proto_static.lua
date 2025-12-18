a0ae4a6316d9f7c2097c59885d458b80d7114b52
Lienol <23146169+Lienol@users.noreply.github.com>
2025-12-19 03:13:02 +0800

    luci: iface: proto_static: sync options with Openwrt 25.12

--- modules/luci-base/luasrc/model/cbi/admin_network/proto_static.lua
+++ modules/luci-base/luasrc/model/cbi/admin_network/proto_static.lua
@@ -6,7 +6,10 @@ local ifc = net:get_interface()
 
 local ipaddr, netmask, gateway, broadcast, ip6addr, ip6gw
 
+local sys = require "luci.sys"
+local old_logic = sys.call('grep "\\.netmask" /bin/config_generate > /dev/null') == 0
 
+if old_logic then
 ipaddr = section:taboption("general", Value, "ipaddr", translate("IPv4 address"))
 ipaddr.datatype = "ip4addr"
 
@@ -18,6 +21,16 @@ netmask.datatype = "ip4addr"
 netmask:value("255.255.255.0")
 netmask:value("255.255.0.0")
 netmask:value("255.0.0.0")
+else
+	ipaddr = section:taboption("general", DynamicList, "ipaddr", translate("IPv4 address"))
+	ipaddr.datatype = "cidr4"
+	ipaddr.placeholder = '192.168.1.1/24'
+
+	ipaddr.write = function(self, section, value)
+		DynamicList.write(self, section, value)
+		map:del(section, "netmask")
+	end
+end
 
 
 gateway = section:taboption("general", Value, "gateway", translate("IPv4 gateway"))
@@ -30,17 +43,14 @@ broadcast.datatype = "ip4addr"
 if luci.model.network:has_ipv6() then
 	ip6addr = section:taboption("general", Value, "ip6addr", translate("IPv6 address"))
 	ip6addr.datatype = "ip6addr"
-	ip6addr:depends("ip6assign", "")
 
 
 	ip6gw = section:taboption("general", Value, "ip6gw", translate("IPv6 gateway"))
 	ip6gw.datatype = "ip6addr"
-	ip6gw:depends("ip6assign", "")
 
 
 	local ip6prefix = s:taboption("general", Value, "ip6prefix", translate("IPv6 routed prefix"),
 		translate("Public prefix routed to this device for distribution to clients."))
 	ip6prefix.datatype = "ip6addr"
-	ip6prefix:depends("ip6assign", "")
 
 end
